3ec06ad143f5edf4330b0d96af845ffe67c3f94d
SuperFeeeelix <127154412+SuperFeeeelix@users.noreply.github.com>
2023-07-02 21:52:35 -0700

    adjusted my tables in my database

--- db/schema.sql
+++ db/schema.sql
@@ -4,11 +4,22 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
-CREATE TABLE category
+CREATE TABLE category (
+id INT NOT NULL AUTO_INCREMENT SET AS PRIMARY KEY,
+category_name STRING NOT NULL
+);
 
 
 
-CREATE TABLE product
+CREATE TABLE product(
+    id INT NOT NULL AUTO_INCREMENT SET AS PRIMARY KEY,
+    product_name STRING NOT NULL,
+    price DECIMAL (10,2) NOT NULL,
+    stock INT NOT NULL,
+    category_id INT,
+    FOREIGN KEY (category_id),
+    REFERENCES  
+)
 
 CREATE TABLE tag
 
