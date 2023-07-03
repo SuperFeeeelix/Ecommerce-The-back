10cf26337f4d35d560f96d5644966251a1a6d77e
SuperFeeeelix <127154412+SuperFeeeelix@users.noreply.github.com>
2023-07-03 12:16:20 -0700

    finished creating my tables on my schema.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -3,24 +3,36 @@ DROP DATABASE IF EXISTS ecommerce_db;
 
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
+USE ecommerce_db;
 
 CREATE TABLE category (
-id INT NOT NULL AUTO_INCREMENT SET AS PRIMARY KEY,
-category_name STRING NOT NULL
+    id INT NOT NULL AUTO_INCREMENT,
+    category_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
 );
 
-
-
 CREATE TABLE product(
-    id INT NOT NULL AUTO_INCREMENT SET AS PRIMARY KEY,
-    product_name STRING NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(255) NOT NULL,
     price DECIMAL (10,2) NOT NULL,
-    stock INT NOT NULL,
+    stock INT NOT NULL DEFAULT 10,
     category_id INT,
-    FOREIGN KEY (category_id),
-    REFERENCES  
-)
+    PRIMARY KEY (id),
+    FOREIGN KEY (category_id)
+    REFERENCES category(id)
+);
 
-CREATE TABLE tag
+CREATE TABLE tag (
+    id INT NOT NULL AUTO_INCREMENT,
+    tag_name VARCHAR(255),
+    PRIMARY KEY (id),
+);
 
-CREATE TABLE productTag
+CREATE TABLE productTag (
+    id INT NOT NULL AUTO_INCREMENT,
+    product_id INT,
+    tag_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (product_id) REFERENCES Product(id),
+    FOREIGN KEY (tag_id) REFERENCES Tag(id)
+);
